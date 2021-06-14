e64365502478ec3a398733e6a3355774517f702e
bassamhamdar <hamdar.m.bassam@gmail.com>
2021-06-14 18:27:30 +0300

    step 4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.listen(3000, ()=>console.log('ok'));
+
 app.get('/test',(req,res)=>{
 res.send({status : 200 ,
     message: "ok"
@@ -12,3 +13,28 @@ app.get('/time',(req,res)=>{
         message: today.getHours()+":"+today.getSeconds()
     })
     });
+
+// step 4
+
+    app.get('/hello/:id?',(req,res)=>{
+        res.send({status : 200 ,
+        message: `hello ${req.params.id}`
+        })
+    });
+    app.get('/search?s=:search',(req,res)=>{
+        res.send({status : 200 ,
+        message : 'ok',
+        data: `${req.params.search}`
+        })
+    });
+
+    app.get('*',(req,res)=>{
+            res.send({
+                status: 500,
+                error: true,
+                message: 'you have to provide a search'
+            })
+        
+
+    });
+
