e285b2a854bbbe66616e5e584af7ac9621490fe5
bassamhamdar <hamdar.m.bassam@gmail.com>
2021-06-14 15:57:56 +0300

    step 3

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const app = express();
 app.listen(3000, ()=>console.log('ok'));
+app.get('/test',(req,res)=>{
+res.send({status : 200 ,
+    message: "ok"
+})
+});
+app.get('/time',(req,res)=>{
+    var today = new Date();
+    res.send({status : 200 ,
+        message: today.getHours()+":"+today.getSeconds()
+    })
+    });
