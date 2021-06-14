df7df4c0a94b470cb23ae87f4d067de10babddba
bassamhamdar <hamdar.m.bassam@gmail.com>
2021-06-14 18:42:40 +0300

    step 5

--- index.js
+++ index.js
@@ -28,13 +28,45 @@ app.get('/time',(req,res)=>{
         })
     });
 
-    app.get('*',(req,res)=>{
-            res.send({
-                status: 500,
-                error: true,
-                message: 'you have to provide a search'
-            })
+    app.get('/search?s=',(req,res)=>{
+        res.send({status : 500 ,
+        error : true,
+        data: 'you have to provide a search'
+        })
+    });
+
+
+    // app.get('*',(req,res)=>{
+    //         res.send({
+    //             status: 500,
+    //             error: true,
+    //             message: 'you have to provide a search'
+    //         })
         
 
+    // });
+
+// step 5
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+app.post('/movies/create', (req,res)=>{
+    res.send('create');
+});
+app.get('/movies/read', (req,res)=>{
+    res.send({
+        status:200,
+        data: movies
     });
+});
+app.put('/movies/update', (req,res)=>{
+    res.send('update');
+});
+app.delete('/movies/delete', (req,res)=>{
+    res.send('delete');
+});
 
